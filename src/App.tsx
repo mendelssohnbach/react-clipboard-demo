a624122a838ba28934c400516b2975b6f011a648
mendelssohnbach <mendelssohnusaic+github@gmail.com>
2022-07-18 06:24:50 +0900

    :sparkles: クリップボードコピーコンポーネント作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import './App.css';
+
+import { CopyToClipboard } from 'react-copy-to-clipboard';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [text, setText] = useState('Sample Text');
+
+  const updateText = (text: string) => {
+    setText(text);
+  };
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <input
+        type="text"
+        onChange={(e) => updateText(e.target.value)}
+        value={text}
+      />
+      <div className="App-copy-area">
+        <CopyToClipboard
+          text={text}
+          onCopy={() => alert(`クリップボードに「${text}」をコピーしました`)}
+        >
+          <button>クリップボードにコピーする</button>
+        </CopyToClipboard>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
